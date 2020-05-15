4d8dcc2f25a4bff14e55a905f42dd2c1d7f0e577
izdine <54841429+izdine@users.noreply.github.com>
2020-05-15 12:21:55 +0200

    post isuse

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<center>
+    <div class="login_container create_post_container">
+        <h3>Post aanmaken</h3>
+        <form action="create_post.php" method="post" id="form">
+            <input type="text" name="image" placeholder="Foto link"><br>
+            <input type="text" name="title" placeholder="Titel"><br>
+            <textarea form="form" width="100" type="text" name="text" placeholder="Text..."></textarea><br>
+            <input type="submit" name="submit">
+        </form>
+    </div>
+</center>
+
 <?php
 $host = '127.0.0.1';
 $db   = 'scrum';
@@ -23,4 +35,28 @@ while($row=$result->fetch()){
     echo "Issue: " . $row['issue'] .'<br>';
 
 }
+?>
+<?php
+if(!isset($_COOKIE["logged_in"])){
+    header("Location: login.php");
+}
+
+if(isset($_POST["submit"])){
+    try {
+        $pdo = new PDO($dsn, $user, $pass, $options);
+        if(!empty($_POST["naam"]) && !empty($_POST["issue"])  {
+            $stmt = $pdo->prepare(
+                "INSERT INTO post (naam, issue, image_link)
+                VALUES ('".$_POST["naam"]."', '".$_POST["issue"]."')"
+            );
+            $stmt->execute();
+            header("Location: index.php");
+        } else {
+            throw new Exception("Je hebt niet alles ingevult");
+        }
+    } catch (Exception $e) {
+        echo "<p>Error: ".$e->getMessage()."</p>";
+    }
+}
+
 ?>
