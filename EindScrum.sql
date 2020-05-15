d7db072269615e96debae1d7b3588767caa7c117
Laurens van der BIlt <54841481+2099492@users.noreply.github.com>
2020-05-15 14:30:11 +0200

    Add files via upload

--- EindScrum.sql
+++ EindScrum.sql
@@ -8,9 +8,4 @@ lokaal varchar(20) NULL,
 ArrivalDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
 id INT AUTO_INCREMENT PRIMARY KEY NOT NULL
 );
-INSERT INTO info(naam, issue, lokaal) value
-('sam', 'probleem 1', 'aarde'),
-('Izdine', 'probleem 2', 'mars'),
-('Laurens', 'probleem 3', 'Jupiter'),
-('Kenza', 'probleem 4', 'Maan');
 
