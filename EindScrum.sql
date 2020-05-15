286cc78ee2921b6c632520875e40bb7a7b99fe63
Laurens van der BIlt <54841481+2099492@users.noreply.github.com>
2020-05-15 13:29:08 +0200

    Add files via upload

--- EindScrum.sql
+++ EindScrum.sql
@@ -1,13 +1,16 @@
+USE scrum;
 DROP TABLE info;
+USE scrum;
 CREATE TABLE info(
 naam varchar(30) NOT NULL,
 issue varchar(500) NOT NULL,
-ArrivalDate datetime NOT NULL,
 lokaal varchar(20) NULL,
+ArrivalDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
 id INT AUTO_INCREMENT PRIMARY KEY NOT NULL
 );
-INSERT INTO info(naam, issue, lokaal, ArrivalDate) value
-('sam', 'probleem 1', 'aarde',  curdate()),
-('Izdine', 'probleem 2', 'mars', curdate()),
-('Laurens', 'probleem 3', 'Jupiter', curdate()),
-('Kenza', 'probleem 4', 'Maan', curdate());
+INSERT INTO info(naam, issue, lokaal) value
+('sam', 'probleem 1', 'aarde'),
+('Izdine', 'probleem 2', 'mars'),
+('Laurens', 'probleem 3', 'Jupiter'),
+('Kenza', 'probleem 4', 'Maan');
+
--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <center>
-    <div class="login_container create_post_container">
+
         <h3>Post aanmaken</h3>
-        <form action="create_post.php" method="post" id="form">
-            <input type="text" name="image" placeholder="Foto link"><br>
-            <input type="text" name="title" placeholder="Titel"><br>
-            <textarea form="form" width="100" type="text" name="text" placeholder="Text..."></textarea><br>
+        <form action="index.php" method="post" id="form">
+            <input type="text" name="naam" placeholder="Titel"><br>
+            <textarea form="form" width="100" type="text" name="issue" placeholder="Text..."></textarea><br>
             <input type="submit" name="submit">
         </form>
-    </div>
+
 </center>
 
 <?php
@@ -32,22 +31,18 @@ $result = $pdo->query('SELECT * FROM info');
 
 while($row=$result->fetch()){
     echo "Naam: " . $row['naam'] . ' ';
-    echo "Issue: " . $row['issue'] .'<br>';
+    echo "Issue: " . $row['issue'] .' ';
+    echo "Tijd: " . $row['ArrivalDate'] . '<br>';
 
 }
 ?>
 <?php
-if(!isset($_COOKIE["logged_in"])){
-    header("Location: login.php");
-}
-
 if(isset($_POST["submit"])){
     try {
-        $pdo = new PDO($dsn, $user, $pass, $options);
         if(!empty($_POST["naam"]) && !empty($_POST["issue"]) ) {
             $stmt = $pdo->prepare(
-                "INSERT INTO post (naam, issue,)
-                VALUES ('".$_POST["naam"]."', '".$_POST["issue"]."')"
+                "INSERT INTO info (naam, issue)
+                VALUES ('".$_POST["naam"]."', '".$_POST["issue"]."');"
             );
             $stmt->execute();
             header("Location: index.php");
