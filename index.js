64dea677c870dc4b84232ed18c627589208ee6a0
Laurens van der BIlt <54841481+2099492@users.noreply.github.com>
2020-06-16 10:57:40 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,75 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host: "127.0.0.1",
-  database: 'scrum',
-  user:  "root",
-  password: ''
+  host: "skil1.mysql.database.azure.com",
+  database: 'skil',
+  user:  "izdine@skil1",
+  password: 'IKLS+1234'
 });
 
 
-
-app.listen('3000', () => {
-  console.log();
-});
 
 connection.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
 });
+
+var SlackBot = require('slackbots');
+var studentsArray = [];
+
+// create a bot
+var bot = new SlackBot({
+    token: 'xoxb-369216512676-1162931326837-zBVtyygexEb8TBvqP2kOsBjd', // Add a bot https://my.slack.com/services/new/bot and put the token
+    name: 'skilbot'
+});
+
+bot.on('start', () => {
+    const params = {
+        icon_emoji: ':robot:'
+    };
+    bot.postMessageToChannel(
+        'skilbot_test_channel',
+        'SKILbot tot je beschikking',
+        params
+      );
+    });
+    function handleMessage(message) {
+        if(message.includes('greet')) {
+            inspireMe()
+        } else if(message.includes('STUDENTS')) {
+          console.log('test');
+            studentsArray.forEach(function(result, i) {
+              bot.postMessageToChannel(
+                  'skilbot_test_channel',
+                  console.log(result)
+                );
+             });
+        } else if(message.includes('help')) {
+            runHelp()
+        }
+    };
+    // Error Handler
+    bot.on('error', err => console.log(err));
+
+
+
+    //functions
+    function students() {
+      var STUDENTS = connection.query("SELECT naamStudent FROM STUDENTS", function STUDENTS (err, result) {
+        if (err) throw err;
+        studentsArray.push(result);
+      });
+    }
+    // Message Handler
+    bot.on('message', data => {
+      if (data.type !== 'message') {
+        return;
+      }
+
+      handleMessage(data.text);
+    });
+
+// connection.query('SELECT * FROM issues', function (error, results, fields) {
+//   if (error) throw error;
+//   console.log(results);
+// });
