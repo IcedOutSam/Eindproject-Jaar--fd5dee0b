1da3fd58c80787c65e3b18ec439e9e55e8000c0c
Laurens van der BIlt <54841481+2099492@users.noreply.github.com>
2020-06-18 16:27:01 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,84 +1,92 @@
-var mysql = require('mysql');
+require('dotenv').config();
+const token = process.env.SLACK_TOKEN;
+const mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host: "skil1.mysql.database.azure.com",
-  database: 'skil',
-  user:  "izdine@skil1",
-  password: 'IKLS+1234'
+host: "skil1.mysql.database.azure.com",
+database: 'skil',
+user:  process.env.DB_USER,
+password: process.env.DB_PASS
 });
 
 
 
 connection.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
+if (err) throw err;
+console.log("Connected!");
 });
 
 var SlackBot = require('slackbots');
-var studentsArray = [];
+const coaches = ["ties", "klaas"];
 
 // create a bot
 var bot = new SlackBot({
-    token: 'xoxb-369216512676-1162931326837-OkyndgX8ZO8soYKLoQfkRJJj', // Add a bot https://my.slack.com/services/new/bot and put the token
-    name: 'skilbot'
+token: process.env.SLACK_TOKEN, // Add a bot https://my.slack.com/services/new/bot and put the token
+name: 'skilbot'
 });
-
 bot.on('start', () => {
     const params = {
         icon_emoji: ':robot:'
     };
+
     bot.postMessageToChannel(
         'skilbot_test_channel',
         'SKILbot tot je beschikking',
         params
       );
     });
-    function handleMessage(message) {
-        if(message.includes('greet')) {
-            // inspireMe()
-        } else if(message.includes('students')) {
-          issuesTies();
-        } else if(message.includes('help')) {
-            // runHelp()
-        }
-    };
 
-    // Error Handler
-    bot.on('error', err => console.log(err));
+bot.on('message', (message) => {
+  if (message.type == 'message') {
+    console.log(message.user);
+  }
+});
+
+function handleMessage(message) {
+    if(message.includes('greet')) {
+        // inspireMe()
+    } else if(message.includes('<@U014STD9LQM> Ties')) {
+      issuesTies();
+    } else if(message.includes('help')) {
+        // runHelp()
+    }
+};
+
+// Error Handler
+bot.on('error', err => console.log(err));
 
 
 
-    //functions
-    function issuesTies() {
-      connection.query("SELECT idIssue FROM coachIssue WHERE idCoach=1", function STUDENTS (err, resultTies) {
-        for (var i = 0; i < resultTies.length; i++) {
-          console.log(resultTies[i].idIssue);
-          var ID = resultTies[i].idIssue;
-          console.log(ID);
+//functions
+function issuesTies() {
+  console.log('begin functie Ties()');
+  connection.query("SELECT idIssue FROM coachIssue WHERE idCoach=1", function STUDENTS (err, resultTies) {
+    for (var i = 0; i < resultTies.length; i++) {
+      console.log(`index: ${resultTies[i].idIssue} in de for loop`);
+      var ID = resultTies[i].idIssue;
 
-      connection.query("SELECT * FROM ISSUES WHERE ID="+ID, function STUDENTS (err, resultIssue) {
-        if (err) throw err;
-        // studentsArray.push(result);
-        for (var i = 0; i < resultIssue.length; i++) {
-        bot.postMessageToChannel(
-            'skilbot_test_channel',
-            resultIssue[i]
-          );
-        }
-      });
+  connection.query("SELECT * FROM ISSUES WHERE ID="+ID, function STUDENTS (err, resultIssue) {
+    if (err) throw err;
+
+    for (var j = 0; j < resultIssue.length; j++) {
+    bot.postMessageToChannel(
+        'skilbot_test_channel',
+        resultIssue[j]
+      );
      }
-    });
-   }
-    // Message Handler
-    bot.on('message', data => {
-      if (data.type !== 'message') {
-        return;
-      }
+   });
+  }
+ });
+}
+// Message Handler
+bot.on('message', data => {
+  if (data.type !== 'message') {
+    return;
+  }
 
-      handleMessage(data.text);
-    });
+  handleMessage(data.text);
+});
 
-// connection.query('SELECT * FROM issues', function (error, results, fields) {
-//   if (error) throw error;
-//   console.log(results);
-// });
+
+//const coaches = ["ties", "klaas"]
+//if (coaches.includes(message.user)) { send msg }
