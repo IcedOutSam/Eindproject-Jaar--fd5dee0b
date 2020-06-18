0896324b1d6556b359bbd46a59a1d1f911a9c8e7
Kenza <k.bennani-meziane@student.bit-academy.nl>
2020-06-18 15:33:01 +0200

    18/06/202

--- index.js
+++ index.js
@@ -1,84 +1,124 @@
-const SlackBot = require('slackbots');
+    const SlackBot = require('slackbots');
+    const dotenv = require('dotenv');
+    const mysql = require('mysql');
 
-
-// Bot Info
-var bot = new SlackBot({
-    token: 'xoxb-369216512676-1162931326837-zBVtyygexEb8TBvqP2kOsBjd', //DO NOT SHARE! // Bot's Token
-    name: 'SKILbot'
-});
-
-// Start Handler
-bot.on('start', () => {
-    bot.postMessageToChannel('skilbot_test_channel', 'SKILbot tot je beschikking!\nHeb je me nodig? Type "SKIL" nadat je me mentioned.'); //First Message sent upon launching, channel name and message
-
-    //Later adding the ability to DM a user    
-
-});
+    var connection = mysql.createConnection({
+    host: "skil1.mysql.database.azure.com",
+    database: 'skil',
+    user:  process.env.DB_USER,
+    password: process.env.DB_PASS
+    });
 
 
 
-// SKIL function "command"
-function handleMessage(message) {
-    if (message.includes(' SKIL')) {
-        responseSKIL();
-    }
-}
+    connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    });
 
-// Message Handler
+    var SlackBot = require('slackbots');
+    var studentsArray = [];
 
-bot.on('message', data => {
-    if (data.type !== 'message') {
-        return;
-    }
+    // create a bot
+        var bot = new SlackBot({
+        token: process, // Add a bot https://my.slack.com/services/new/bot and put the token
+        name: 'skilbot'
+        });
 
-    handleMessage(data.text);
-});
+        bot.on('start', () => {
+        const params = {
+            icon_emoji: ':robot:'
+        };
+        bot.postMessageToChannel(
+            'skilbot_test_channel',
+            'SKILbot tot je beschikking',
+            params
+        );
+        });
+        function handleMessage(message) {
+            if(message.includes('greet')) {
+                // inspireMe()
+            } else if(message.includes('students')) {
+            issuesTies();
+            } else if(message.includes('help')) {
+                // runHelp()
+            }
+        };
 
-// Bot's reply to the Greeting
-function responseSKIL() {
-    bot.postMessageToChannel('skilbot_test_channel', "yo")
-}
+        // Error Handler
+        bot.on('error', err => console.log(err));
 
-// Attachment Preview Message
-// {
-// 	"attachments": [
-// 		{
-// 			"blocks": [
-// 				{
-// 					"type": "section",
-// 					"text": {
-// 						"type": "mrkdwn",
-// 						"text": "Hallo, ik ben SKILbot! Momenteel ben ik nog een prototype en wordt er nog verder aan mij gewerkt. Wil je wat meer weten over de school waar mijn developers opzitten? Klik op de knop! Tot ziens! uwu"
-// 					},
-// 					"accessory": {
-// 						"type": "image",
-// 						"image_url": "https://pbs.twimg.com/profile_images/1083376501939208193/es-8Ejer_400x400.jpg",
-// 						"alt_text": "Bit Academy"
-// 					}
-// 				},
-// 				{
-// 					"type": "actions",
-// 					"elements": [
-// 						{
-// 							"type": "button",
-// 							"text": {
-// 								"type": "plain_text",
-// 								"text": "Ga naar site"
-// 							},
-// 							"value": "https://www.bit-academy.nl/"
-// 						}
-// 					]
-// 				},
-// 				{
-// 					"type": "context",
-// 					"elements": [
-// 						{
-// 							"type": "plain_text",
-// 							"text": "SKILbot© is powered by Sam, Kenza, Iz-Dine and Laurens."
-// 						}
-// 					]
-// 				}
-// 			]
-// 		}
-// 	]
-// }
+        //Functions
+        function issuesTies() {
+        connection.query("SELECT idIssue FROM coachIssue WHERE idCoach=1", function STUDENTS (err, resultTies) {
+            for (var i = 0; i < resultTies.length; i++) {
+            console.log(resultTies[i].idIssue);
+            var ID = resultTies[i].idIssue;
+            console.log(ID);
+
+        connection.query("SELECT * FROM ISSUES WHERE ID="+ID, function STUDENTS (err, resultIssue) {
+            if (err) throw err;
+        
+            for (var i = 0; i < resultIssue.length; i++) {
+            bot.postMessageToChannel(
+                'skilbot_test_channel',
+                resultIssue[i]
+            );
+            }
+        });
+        }
+        });
+        }
+    
+        // Message Handler
+        bot.on('message', data => {
+        if (data.type !== 'message') {
+            return;
+        }
+
+        handleMessage(data.text);
+        });
+
+    // Attachment Preview Message
+    // {
+    // 	"attachments": [
+    // 		{
+    // 			"blocks": [
+    // 				{
+    // 					"type": "section",
+    // 					"text": {
+    // 						"type": "mrkdwn",
+    // 						"text": "Hallo, ik ben SKILbot! Momenteel ben ik nog een prototype en wordt er nog verder aan mij gewerkt. Wil je wat meer weten over de school waar mijn developers opzitten? Klik op de knop! Tot ziens! uwu"
+    // 					},
+    // 					"accessory": {
+    // 						"type": "image",
+    // 						"image_url": "https://pbs.twimg.com/profile_images/1083376501939208193/es-8Ejer_400x400.jpg",
+    // 						"alt_text": "Bit Academy"
+    // 					}
+    // 				},
+    // 				{
+    // 					"type": "actions",
+    // 					"elements": [
+    // 						{
+    // 							"type": "button",
+    // 							"text": {
+    // 								"type": "plain_text",
+    // 								"text": "Ga naar site"
+    // 							},
+    // 							"value": "https://www.bit-academy.nl/"
+    // 						}
+    // 					]
+    // 				},
+    // 				{
+    // 					"type": "context",
+    // 					"elements": [
+    // 						{
+    // 							"type": "plain_text",
+    // 							"text": "SKILbot© is powered by Sam, Kenza, Iz-Dine and Laurens."
+    // 						}
+    // 					]
+    // 				}
+    // 			]
+    // 		}
+    // 	]
+    // }
